4e8f61a5c0bc95bcebd397bedbc1151378ab5e59
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2022-05-06 11:05:58 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+// use dotenv to read the ENV variables from the .env file
 require("dotenv").config();
+// abort if the Google API key location is not present as an ENV variable
 if (!process.env.GOOGLE_APPLICATION_CREDENTIALS || process.env.GOOGLE_APPLICATION_CREDENTIALS === "") {
   console.error("Error: GOOGLE_APPLICATION_CREDENTIALS env variable not set! Aborting...");
   process.exit(1);
@@ -6,13 +8,13 @@ if (!process.env.GOOGLE_APPLICATION_CREDENTIALS || process.env.GOOGLE_APPLICATIO
 
 async function main() {
   // import the Google Cloud client library
-  const vision = require("@google-cloud/vision");
+  
   // create a client
-  const client = new vision.ImageAnnotatorClient();
+  
+  // use client to check the images in `./resources/`
 
-  const fileName = "e83db50d29f4073ed1584d05fb1d4e9fe777ead218ac104497f5c978a6ebb3bf_640.jpg";
-  const [result] = await client.labelDetection(`./resources/${fileName}`);
-  console.log(result.labelAnnotations);
+  // output results
+  
 }
 
 // execute main function
