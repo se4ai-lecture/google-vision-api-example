3860f4563c517231dfb66681f92d9bc8ff1b138c
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2023-04-19 10:44:52 +0200

    Updated dependencies and improved comments in sample solution

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ if (!process.env.GOOGLE_APPLICATION_CREDENTIALS || process.env.GOOGLE_APPLICATIO
   process.exit(1);
 }
 
+// iterates through a label annotations array and returns the individual label if a cat is found; returns null if no cat is found
 function getCatLabel(labels) {
   let catLabel = null;
   labels.forEach((label) => {
@@ -25,10 +26,12 @@ async function main() {
   // iterate over list of image file names
   const fs = require("fs");
   fs.readdir("./resources", async (err, files) => {
+    // send an API request for each file name
     const promises = files.map((file) => client.labelDetection(`./resources/${file}`));
-
+    // wait until all requests have resolved, then iterate over the results
     Promise.all(promises).then((results) => {
       results.forEach((result, index) => {
+        // check if there is a cat label in the annotations (if yes, save it to the catPredictions array)
         const catLabel = getCatLabel(result[0].labelAnnotations);
         if (catLabel) {
           catPredictions.push({
@@ -37,7 +40,8 @@ async function main() {
           });
         }
       });
-      console.log(`The image set contains ${catPredictions.length} cats:`);
+      // print out the results
+      console.log(`The image set contains ${catPredictions.length} cats.`);
       catPredictions.forEach((cat) => console.log(`- ${cat.fileName} (${cat.score.toFixed(3)})`));
     });
   });
